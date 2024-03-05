10af57b88f8c5827cbf1ca49fbc74bc6f45ee1d9
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-03-05 20:04:16 +0200

    load person details

--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -1,8 +1,14 @@
+import { Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom'
+import usePerson from '../hooks/usePerson';
+import PersonDetails from '../components/PersonDetails';
 
 const PersonPage = () => {
   const { id } = useParams()
-  return <div>person {id}</div>
+  const { data: person, isLoading, error } = usePerson(id!);
+  if (isLoading) return <Spinner />;
+  if (error || !person) throw error;
+  return <PersonDetails person={person}/>
 }
 
 export default PersonPage
