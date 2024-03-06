c5d864ed826a3fc1cb656f3d767650ce525dce86
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-03-06 08:59:27 +0200

    style category list

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,15 +1,24 @@
-import { Box, Card, CardBody, CardHeader, Spinner } from '@chakra-ui/react'
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Spinner,
+  Stack,
+} from '@chakra-ui/react'
 import { Link, useSearchParams } from 'react-router-dom'
-import useSearch from '../hooks/useSearch'
-import { Category } from './Categories'
-import CategoryDivider from './CategoryDivider'
-import { getRedirectLink } from '../utils/getRedirectLink'
 import Film from '../entities/Film'
 import Person from '../entities/Person'
 import Planet from '../entities/Planet'
 import Specie from '../entities/Specie'
 import Starship from '../entities/Starship'
 import Vehicle from '../entities/Vehicle'
+import useSearch from '../hooks/useSearch'
+import { getRedirectLink } from '../utils/getRedirectLink'
+import { Category } from './Categories'
+import CategoryDivider from './CategoryDivider'
+import Films from './Films'
 
 const CategoryList = ({ category }: { category: Category }) => {
   const [searchParams] = useSearchParams()
@@ -25,6 +34,14 @@ const CategoryList = ({ category }: { category: Category }) => {
     }
     return item.title
   }
+  const getFilms = (
+    item: Film | Person | Planet | Vehicle | Specie | Starship
+  ): string[] => {
+    if ('films' in item) {
+      return item.films
+    }
+    return []
+  }
   return (
     <Card m={2}>
       <CardHeader>
@@ -32,9 +49,29 @@ const CategoryList = ({ category }: { category: Category }) => {
       </CardHeader>
       <CardBody>
         {data?.results.map((item) => (
-          <Box key={getValue(item)} paddingLeft={5}>
-            <Link to={getRedirectLink(item.url)}>{getValue(item)}</Link>
-          </Box>
+          <Card
+            direction={{ base: 'column', sm: 'row' }}
+            overflow='hidden'
+            variant='outline'
+            key={getValue(item)}
+          >
+            <Stack>
+              <CardBody>
+                <Heading size='md'>
+                  {' '}
+                  <Link to={getRedirectLink(item.url)}>{getValue(item)}</Link>
+                </Heading>
+                <Box pt='2' fontSize='sm'>
+                  <Films
+                    films={getFilms(item).map((film) =>
+                      film.replace(/\D/g, '')
+                    )}
+                    label={'Appears in: '}
+                  />
+                </Box>
+              </CardBody>
+            </Stack>
+          </Card>
         ))}
       </CardBody>
     </Card>
