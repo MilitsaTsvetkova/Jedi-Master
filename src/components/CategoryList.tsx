68e12d8b174dd8f46347996c8deed2551a47010f
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-03-06 08:38:16 +0200

    fix type error

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,15 +1,30 @@
-import { Box, Card, CardBody, CardHeader } from '@chakra-ui/react'
+import { Box, Card, CardBody, CardHeader, Spinner } from '@chakra-ui/react'
 import { Link, useSearchParams } from 'react-router-dom'
 import useSearch from '../hooks/useSearch'
 import { Category } from './Categories'
 import CategoryDivider from './CategoryDivider'
 import { getRedirectLink } from '../utils/getRedirectLink'
+import Film from '../entities/Film'
+import Person from '../entities/Person'
+import Planet from '../entities/Planet'
+import Specie from '../entities/Specie'
+import Starship from '../entities/Starship'
+import Vehicle from '../entities/Vehicle'
 
 const CategoryList = ({ category }: { category: Category }) => {
   const [searchParams] = useSearchParams()
   const searchQuery = searchParams.get('q') ?? ''
-  const { data } = useSearch(category.value, searchQuery)
-
+  const { data, isLoading, error } = useSearch(category.value, searchQuery)
+  if (isLoading) return <Spinner />
+  if (error) throw error
+  const getValue = (
+    item: Film | Person | Planet | Vehicle | Specie | Starship
+  ): string => {
+    if ('name' in item) {
+      return item.name as string
+    }
+    return item.title
+  }
   return (
     <Card m={2}>
       <CardHeader>
@@ -17,8 +32,8 @@ const CategoryList = ({ category }: { category: Category }) => {
       </CardHeader>
       <CardBody>
         {data?.results.map((item) => (
-          <Box key={item.name} paddingLeft={5}>
-            <Link to={getRedirectLink(item.url)}>{item.name}</Link>
+          <Box key={getValue(item)} paddingLeft={5}>
+            <Link to={getRedirectLink(item.url)}>{getValue(item)}</Link>
           </Box>
         ))}
       </CardBody>
--- src/entities/Search.ts
+++ src/entities/Search.ts
@@ -1,14 +1,13 @@
-import Film from './Film';
-import Person from './Person';
-import Planet from './Planet';
-import Specie from './Specie';
-import Starship from './Starship';
-import Vehicle from './Vehicle';
+import Film from './Film'
+import Person from './Person'
+import Planet from './Planet'
+import Specie from './Specie'
+import Starship from './Starship'
+import Vehicle from './Vehicle'
 
-export default interface SearchResult{
-    
-        count: number,
-        next: string | null,
-        previous: string | null,
-        results: Person[] | Planet[] | Vehicle[] | Specie[] | Starship[] | Film[]
-    }
+export default interface SearchResult {
+  count: number
+  next: string | null
+  previous: string | null
+  results: Person[] | Planet[] | Vehicle[] | Specie[] | Starship[] | Film[]
+}
